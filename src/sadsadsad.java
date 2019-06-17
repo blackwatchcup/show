704d17585b1b32e246eb86d375acc85f3395bd2b
张智伟 <zhangzhiweiChange@gmail.com>
2019-06-17 16:59:55 +0800

    测试

---
+++ src/sadsadsad.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) Zzw Corp.
+ * All Rights Reserved.
+ */
+
+/**
+ * sadsadsad.
+ *
+ * @author Zhang ZhiWei, 2019年06月17日
+ * @version jpa v1.0
+ */
+public class sadsadsad {
+}
---
+++ src/test.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) Zzw Corp.
+ * All Rights Reserved.
+ */
+
+/**
+ * test.
+ *
+ * @author Zhang ZhiWei, 2019年06月17日
+ * @version jpa v1.0
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("1111111");
+    }
+}
